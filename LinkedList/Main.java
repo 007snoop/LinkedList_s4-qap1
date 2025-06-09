317c76a522591fda74c0ad8b571a7eb20fa9fb53
Colin Yetman <colinguitarist@gmail.com>
2025-06-09 13:17:16 -0230

    Initial commit + add delete method need to debug as list is not updating correctly

---
+++ LinkedList/Main.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SingleLinkedListTest sll = new SingleLinkedListTest();
+        sll.insertLinkedList(1,0);
+        sll.insertLinkedList(2,1);
+        sll.insertLinkedList(3,2);
+        sll.insertLinkedList(4,3);
+        System.out.println("before deletion.");
+        sll.traverseLinkedList();
+        sll.delete(2);
+        System.out.println("after deletion.");
+        sll.traverseLinkedList();
+        sll.searchNode(5);
+
+    }
+}
---
+++ LinkedList/SingleLinkedList.java
@@ -0,0 +1,82 @@
+package LinkedList;
+
+public class SingleLinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
+
+    public Node createSingleLinkedlist(int nodeValue){
+        Node node = new Node();
+        node.next = null;
+        node.value = nodeValue;
+        head = node;
+        tail = node;
+        size = 1;
+        return head;
+    }
+
+    //Insert Method
+
+    public void insertLinkedList(int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+
+        if (head == null){
+            createSingleLinkedlist(nodeValue);
+        } else if (location == 0) {
+            node.next = head;
+            head = node;
+
+        } else if (location >= size){
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < location - 1){
+                tempNode = tempNode.next;
+                index++;
+            }
+            Node nextNode = tempNode.next;
+            tempNode.next = node;
+            node.next = nextNode;
+        }
+
+    }
+
+    public void delete(int pos) {
+        if (head == null) {
+            System.out.println("List empty. Nothing to delete.");
+            return;
+        }
+        if (pos < 0 || pos >= size) {
+            System.out.println("Invalid position.");
+            return;
+        }
+        if (pos == 0) {
+            head = head.next;
+            size--;
+            if (size == 0) {
+                tail = null;
+            }
+            return;
+        }
+
+        Node tempNode = head;
+        int index = 0;
+        while (index < pos - 1) {
+            tempNode = tempNode.next;
+            index++;
+        }
+
+        tempNode.next = tempNode.next.next;
+
+        if (pos == size - 1) {
+            tail = tempNode;
+        }
+        size--;
+    }
+
+}
+
